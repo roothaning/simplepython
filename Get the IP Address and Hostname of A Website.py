152487272e28c02f4696ec2b15598bd92309076d
roothaning <72936244+roothaning@users.noreply.github.com>
2023-01-06 22:22:42 +0300

    Add files via upload

---
+++ Get the IP Address and Hostname of A Website.py
@@ -0,0 +1,16 @@
+# Get Ipaddress and Hostname of Website
+# importing socket library
+import socket
+import os
+
+def get_hostname_IP():
+    hostname = input("Please enter website address(URL):")
+    try:
+        print (f'Hostname: {hostname}')
+        print (f'IP: {socket.gethostbyname(hostname)}')
+    except socket.gaierror as error:
+        print (f'Invalid Hostname, error raised is {error}')
+
+get_hostname_IP()
+
+os.system('pause')
